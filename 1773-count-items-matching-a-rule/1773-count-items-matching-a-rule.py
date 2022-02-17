64e9ebf65721a52be0a190c59e8357937ddea86e
navyanetan <60137417+navyanetan@users.noreply.github.com>
2022-02-18 01:07:23 +0530

    Time: 498 ms (6.33%), Space: 20.3 MB (76.50%) - LeetHub

--- 1773-count-items-matching-a-rule/1773-count-items-matching-a-rule.py
+++ 1773-count-items-matching-a-rule/1773-count-items-matching-a-rule.py
@@ -1,15 +1,29 @@
 class Solution:
     def countMatches(self, items: List[List[str]], ruleKey: str, ruleValue: str) -> int:
-        s = 0
-        for i in range(len(items)):
-            if ruleKey == "type":
-                if ruleValue == items[i][0]:
-                    s += 1
-            if ruleKey == "color":
-                if ruleValue == items[i][1]:
-                    s += 1
-            if ruleKey == "name":
-                if ruleValue == items[i][2]:
-                    s += 1 
-        return s
+        # s = 0
+        # for i in range(len(items)):
+        #     if ruleKey == "type":
+        #         if ruleValue == items[i][0]:
+        #             s += 1
+        #     if ruleKey == "color":
+        #         if ruleValue == items[i][1]:
+        #             s += 1
+        #     if ruleKey == "name":
+        #         if ruleValue == items[i][2]:
+        #             s += 1 
+        # return s
+        if ruleKey == 'type':
+            return [items[i][0] for i in range(len(items))].count(ruleValue)
+        if ruleKey == 'color':
+            return [items[i][1] for i in range(len(items))].count(ruleValue)
+        if ruleKey == 'name':
+            return [items[i][2] for i in range(len(items))].count(ruleValue)
+        # for i in range(len(items)):
+        #     if ruleKey == "type":
+        #         return items[i][0].count(ruleValue)
+        #     if ruleKey == "color":
+        #         return items[i][1].count(ruleValue)    
+        #     if ruleKey == "name" :
+        #         return items[i][2].count(ruleValue)
+
        

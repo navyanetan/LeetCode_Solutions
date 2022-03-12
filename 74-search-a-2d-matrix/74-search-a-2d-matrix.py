3efa3eebfd5b6a01ec0951bf04ae6d478f56a5cb
navyanetan <60137417+navyanetan@users.noreply.github.com>
2022-03-13 00:26:40 +0530

    Time: 29 ms (83.80%), Space: 13.8 MB (48.84%) - LeetHub

--- 74-search-a-2d-matrix/74-search-a-2d-matrix.py
+++ 74-search-a-2d-matrix/74-search-a-2d-matrix.py
@@ -1,12 +1,31 @@
 class Solution(object):
-    def searchMatrix(self, matrix, target):
-        """
-        :type matrix: List[List[int]]
-        :type target: int
-        :rtype: bool
-        """
-        for i in range(0,len(matrix)):
-            for j in range(0,len(matrix[i])):
-                if target == matrix[i][j]:
-                    return True
-        return False
+    def binarysearch(self,arr,target):
+        if len(arr) == 1 and arr[0]!=target:
+            return False
+        center_index = len(arr)//2
+        if target ==arr[center_index]:
+            return True
+        if target < arr[center_index]:
+            return self.binarysearch(arr[:center_index],target)
+        elif target> arr[center_index]:
+            return self.binarysearch(arr[center_index:],target) 
+        
+    def searchMatrix(self, matrix, target):        
+        if len(matrix)==0:
+            return False
+        i = len(matrix)//2
+        row = matrix[i]
+        if target == row[0]:
+            return True
+        if target == row[-1]:
+            return True
+        elif target > row[-1]:
+            return self.searchMatrix(matrix[i+1:], target)              
+        elif target < row[0]:
+            return self.searchMatrix(matrix[:i], target)
+        else:
+            return self.binarysearch(row, target)
+     
+
+        
+    
